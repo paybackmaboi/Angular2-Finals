19a5c7591bef72310eba50a89277f08112f8c07a
Raquel Pacure <raquelpacure6@gmail.com>
2025-05-01 18:16:07 -0700

    added  manage department

---
+++ src/app/admin/departments/departments-routing.module.ts
@@ -0,0 +1,17 @@
+
+//import { NgModule } from '@angular/core';
+//import { RouterModule, Routes } from '@angular/router';
+//import { ListComponent } from './list.component';
+//import { AddEditComponent } from './add-edit.component';
+
+//const routes: Routes = [
+//  { path: 'departments', component: ListComponent },
+//  { path: 'departments/add', component: AddEditComponent },
+//  { path: 'departments/edit/:id', component: AddEditComponent }
+//];
+
+//@NgModule({
+//  imports: [RouterModule.forChild(routes)],
+//  exports: [RouterModule]
+//})
+//export class DepartmentsRoutingModule { }
---
+++ src/app/admin/departments/departments.module.ts
@@ -0,0 +1,20 @@
+//import { NgModule } from '@angular/core';
+//import { ReactiveFormsModule } from '@angular/forms';
+//import { CommonModule } from '@angular/common';
+
+//import { DepartmentsRoutingModule } from './departments-routing.module';
+//import { ListComponent } from './list.component';
+//import { AddEditComponent } from './add-edit.component';
+
+//@NgModule({
+//    imports: [
+//        CommonModule,
+//        ReactiveFormsModule,
+//        DepartmentsRoutingModule
+ //   ],
+   // declarations: [
+   //     ListComponent,
+   //     AddEditComponent
+   // ]
+//})
+//export class DepartmentsModule { }
